c0d3554fea37fa319854aaa531dff5f1e0cfca2f
Mike <mike@traveldepartment.ie>
2018-02-26 14:08:58 +0000

    General Foundation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { OverviewComponent } from './overview/overview.component';
+import { EducationComponent } from './education/education.component';
+import { WorkComponent } from './work/work.component';
+import { MiscellaneousComponent } from './miscellaneous/miscellaneous.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    OverviewComponent,
+    EducationComponent,
+    WorkComponent,
+    MiscellaneousComponent
   ],
   imports: [
     BrowserModule
